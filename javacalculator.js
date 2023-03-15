d6a06bbafb0f0fd34603c4ede7bd545707990bae
asish <87756049+asish-op@users.noreply.github.com>
2023-03-15 18:36:13 +0530

    Create javacalculator.js

---
+++ javacalculator.js
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = input.next().charAt(0);
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        double result;
+        switch(op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
